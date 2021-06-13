2c6848415607700137fe7bd9bbb04e5b7267060f
aadityajo <aadityajoshi2699@gmail.com>
2021-06-13 23:49:47 +0530

    populate answer updated

--- base_app/populate_users.py
+++ base_app/populate_users.py
@@ -1,7 +1,8 @@
 import os
 import django
 import pandas as pd
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base_app.settings')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "base_app.settings")
 django.setup()
 
 from treasurehunt.models import UserProfile, Score
@@ -12,26 +13,24 @@ def remove(string):
 
 
 def populate():
-    df = pd.read_csv("Project Sahara (Responses).csv")
+    df = pd.read_csv("hackathon.csv")
     print(len(df))
     # for i in range(len(data)):
-
     # u = AnswerChecker.objects.get_or_create(email=)[0]
-    for ind in df.index:
-        # print(df['Full Name'][ind], df['Email address'][ind],
-        #       df['Date of Birth'][ind])
-        u = UserProfile.objects.get_or_create(email=remove(
-            df['Email address'][ind]),
-                                              name=df['Full Name'][ind])[0]
+    for ind in df.index[:1]:
+        u = UserProfile.objects.get_or_create(
+            email=remove(df["Email"][ind].lower().strip()),
+            name=df["Name"][ind],
+        )[0]
         u.save()
-        u.set_password(df['Date of Birth'][ind])
+        u.set_password("ecell@12")
         u.save()
         score = Score()
         score.user = u
         score.save()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("populating script")
     populate()
     print("populating complete")
